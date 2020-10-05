051ae8e84a71626a80de38d15e802aed96dd6f0b
Andres Suarez <andres27059934@gmail.com>
2020-10-05 17:01:37 -0300

    feat: test GET /vehicle/:vehicleId

--- src/test/vehicles.spec.ts
+++ src/test/vehicles.spec.ts
@@ -3,26 +3,21 @@ import { app } from '../api/app'
 import moxios from 'moxios'
 import { vehicleData } from "./stubData"
 import { dropDatabaseMongoTestDB } from "./testDatabaseConfig"
-import { getItemCountById } from 'services/databaseAPI/itemDatabase.service'
+import { setDBItemCount } from 'services/databaseAPI/itemDatabase.service'
 
 beforeEach(async () => {
     moxios.install()
-    return dropDatabaseMongoTestDB()
 })
 
 describe('When you send a GET /vehicles/:vehicleID', () => {
 
-    beforeAll(() => {
+    beforeAll(async () => {
         moxios.uninstall()
-
-        moxios.stubRequest(/vehicles.*/, {
-            status: 404,
-            response: '{"details": "Not Found"}'
-        })
+        await dropDatabaseMongoTestDB()
     })
 
     it("Should fail if the ID is not numeric", async () => {
-
+        moxios.stubRequest(/vehicles.*/, { status: 404, response: '{"details": "Not Found"}' })
         const result = await request(app).get('/vehicles/asd').send()
 
         expect(result.status).toBe(404)
@@ -30,39 +25,54 @@ describe('When you send a GET /vehicles/:vehicleID', () => {
 
 
     it('Should fail if the ID is not found', async () => {
-
+        moxios.stubRequest(/vehicles.*/, { status: 404, response: '{"details": "Not Found"}' })
         const result = await request(app).get('/vehicles/9').send()
 
         expect(result.status).toBe(404)
     })
 
-    describe("And you didn't set on the DB the counter for that vehicle", () => {
+    describe("And you didn't set the DB counter for that vehicle", () => {
 
         beforeAll(async () => {
             moxios.uninstall()
-
-            moxios.stubRequest('https://swapi.dev/api/vehicles/8', {
-                status: 200,
-                response: vehicleData[0]
-            })
+            await dropDatabaseMongoTestDB()
         })
 
-
         it('Should return a status 200 OK', async () => {
-
-
-
+            moxios.stubRequest(/vehicles.*/, { status: 200, response: vehicleData[0] })
             const result = await request(app).get('/vehicles/8').send()
 
             expect(result.status).toBe(200)
         })
 
-        it('The database count should be 0 ', async () => {
-
-            await request(app).get('/vehicles/8').send()
-            const count = await getItemCountById('8', 'vehicles')
+        it('The count should be 0', async () => {
+            moxios.stubRequest(/vehicles.*/, { status: 200, response: vehicleData[0] })
+            const result = await request(app).get('/vehicles/8').send()
+            const count = JSON.parse(result.text).count
 
             expect(count).toBe(0)
+        })
+    })
+
+    describe("And you did set 20 on the DB counter for that vehicle", () => {
+        beforeAll(async () => {
+            moxios.uninstall()
+            await dropDatabaseMongoTestDB()
+            await setDBItemCount({ _id: '8', count: 20 }, 'vehicles')
+        })
+
+        it('Should return a status 200 OK', async () => {
+            moxios.stubRequest('https://swapi.dev/api/vehicles/8', { status: 200, response: vehicleData[0] })
+            const result = await request(app).get('/vehicles/8').send()
+            expect(result.status).toBe(200)
+        })
+
+        it('The database count should be 20', async () => {
+            moxios.stubRequest('https://swapi.dev/api/vehicles/8', { status: 200, response: vehicleData[0] })
+            const result = await request(app).get('/vehicles/8').send()
+            const count = JSON.parse(result.text).count
+
+            expect(count).toBe(20)
         })
     })
 })
